6c43bdd6c586077ff1236e1ed55906e0ab916e0b
vk0808 <vk4765712@gmail.com>
2021-09-01 09:52:05 +0530

    feat: create empty board

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -8,6 +8,13 @@ namespace TicTacToeGame
         {
             // Welcome message
             Console.WriteLine("Welcome to Tic-Tac-Toe Game");
+
+            // Object instantiation
+            TicTacToeGame game = new TicTacToeGame();
+
+            // Calling method of TicTacToe class
+            game.CreateBoard();
+
         }
     }
 }
---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToeGame
+    {
+        // instance variable
+        public char[] board;
+
+        // constructor
+        public TicTacToeGame()
+        {
+            board = new char[11];
+        }
+
+        // method to create empty board
+        public void CreateBoard()
+        {
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = '-';
+            }
+        }
+    }
+}
